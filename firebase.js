a86f2c80d06407c29afa0b6a87d94d9ede7d3f4e
NasirAli1419 <1419nasir@gmail.com>
2021-06-11 16:47:32 +0530

    Completed Authentication using nextAuth and Completed add/remove from basket

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYWJBsxE9RNY3v_NCYM_HyQXEWcSI8zb8",
+  authDomain: "clone-cf4df.firebaseapp.com",
+  projectId: "clone-cf4df",
+  storageBucket: "clone-cf4df.appspot.com",
+  messagingSenderId: "691476826260",
+  appId: "1:691476826260:web:0749a0f0379839ffa5cc81",
+};
